b95098f96c9b511fa4b593e7e1e39c7aa6baad6d
yukito.takeuchi <iyggf66974@yahoo.ne.jp>
2024-12-31 19:58:08 +0900

    update ok

--- core/urls.py
+++ core/urls.py
@@ -6,6 +6,7 @@ from core import views
 urlpatterns = [
     path('index',views.index,name='index'),
     path('post/<int:pk>',views.detail,name='detail'),
-    path('create',views.create),
+    path('create',views.create,name='create'),
+    path('update/<int:pk>',views.update,name='update'),
 
 ]
