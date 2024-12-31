31dd1ca04bd2012f911e6aaaa9a8842151f57bce
yukito.takeuchi <iyggf66974@yahoo.ne.jp>
2024-12-31 16:42:44 +0900

    PostModel ok

--- core/models.py
+++ core/models.py
@@ -1,3 +1,12 @@
-from django.db import models
 
-# Create your models here.
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Post(models.Model):
+    user=models.ForeignKey(User,on_delete=models.CASCADE)
+    text=models.TextField(max_length=300)
+    date=models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.text
